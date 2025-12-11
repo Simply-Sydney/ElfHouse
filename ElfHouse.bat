91adf0fe4dc8ebb500b5b4453fb5ee0abb83a340
Simply-Sydney <96808257+Simply-Sydney@users.noreply.github.com>
2025-12-11 18:40:02 -0500

    Initial Creation
    Initial document and repo creation and upload to github

---
+++ ElfHouse.bat
@@ -0,0 +1,24 @@
+::
+:: Elfy's House Adventure.
+::
+:: Version history:
+::   0.01 - 2025/12/11
+::      Initial document creation, introduction and description
+
+@echo off
+
+CLS
+echo Disclaimer: This will look better in fullscreen
+SET /P CONTINUE=Press ENTER to continue.
+CLS
+echo Hello, and welcome to my house.
+echo In this script you will walk around my residence, and maybe through a little bit of magic, feel like we were there together through spacetime - close in spirit, if far in proximity.
+echo There may be some hidden options from time to time, paths will obviously be expanded as I continue working on this
+echo.
+echo Please enjoy! And if you encounter bugs or have suggestions, reach out to me. Yall know where I live after all~~
+echo     -- Love, Elfy
+echo.
+echo.
+echo.
+SET /P CONTINUE=Press ENTER to continue.
+CLS
